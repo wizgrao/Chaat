82eb929d0575f85c7b7cadc2fdb1ac1a19efd0cc
Gaurav Rao <wizgrao@gmail.com>
2015-02-22 11:44:15 -0500

    Improved chaat

--- Chaat.java
+++ Chaat.java
@@ -1,15 +1,15 @@
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
-import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.HashMap;
 import java.util.Scanner;
 import java.util.Vector;
 
 public class Chaat {
 	static Vector<PrintStream> connections;
+	static HashMap<String, String> names;
 	public static void main(String[] args) {
 		connections = new Vector();
 		try {
@@ -30,23 +30,37 @@ public class Chaat {
 		public Handler(Socket s) {
 			this.s = s;
 		}
+		synchronized void say(String message) {
+			for (PrintStream p : connections) {
+				p.println(message);
+			}
+		}
 		@Override
 		public void run() {
-			System.out.println("Connected");
+			String name = "";
+
 			PrintStream out;
 
 			try {
 				out = new PrintStream(s.getOutputStream());
 				connections.add(out);
-
+				boolean newName = false;
 				Scanner b = new Scanner(new InputStreamReader(
 						s.getInputStream()));
+				while (!newName) {
+					out.println("Enter Username: ");
+					name = b.nextLine();
+					if (!names.containsKey(name)) {
+						names.put(name, name);
+						connections.add(out);
+						say(name + " connected");
+						newName = true;
+					}
+				}
 
 				while (b.hasNextLine()) {
 					String sss = b.nextLine();
-					for (PrintStream p : connections) {
-						p.println(sss);
-					}
+					say(name + ": " + sss);
 				}
 				s.close();
 				out.close();
@@ -60,3 +74,4 @@ public class Chaat {
 
 	}
 }
+
